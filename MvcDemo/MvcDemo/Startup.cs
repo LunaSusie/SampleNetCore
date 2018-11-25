c4c09641574f45ca15f11a386588d4146f39a3c2
LunaSelene <339698418@qq.com>
2018-11-25 19:12:23 +0800

    反向代理参数设置

--- MvcDemo/MvcDemo/Startup.cs
+++ MvcDemo/MvcDemo/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using MvcDemo.Service;
 using MvcDemo.Service.MemoryService;
@@ -57,6 +58,16 @@ namespace MvcDemo
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            //反向代理ForwardedHeaders
+            // if (env.IsProduction())
+            // {
+            //       app.UseForwardedHeaders(new ForwardedHeadersOptions
+            //    {
+            //     ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedFor
+            //  });
+            // }
+
             //静态页面
             app.UseStaticFiles();
             //使用状态码页面
