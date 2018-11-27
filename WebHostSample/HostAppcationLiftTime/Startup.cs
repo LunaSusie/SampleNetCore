80a8d65a2b1e7f5ef4a9f5eed7f8b956763e8c70
LunaSelene <339698418@qq.com>
2018-11-28 01:47:03 +0800

    webhost的一些配置

---
+++ WebHostSample/HostAppcationLiftTime/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HostAppcationLiftTime
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IApplicationLifetime applicationLifetime)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            applicationLifetime.ApplicationStarted.Register(() =>
+            {
+                Console.WriteLine("host started");
+            });
+            applicationLifetime.ApplicationStopping.Register(() =>
+            {
+                Console.WriteLine("host Stopping");
+            });
+            applicationLifetime.ApplicationStopped.Register(() =>
+            {
+                Console.WriteLine("host Stopped");
+            });
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
